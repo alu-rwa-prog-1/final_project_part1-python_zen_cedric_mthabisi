ff5ab9c7d3cc05502abb93c0aa8da46f9f2dc9c0
Cedric Murairi <murairicedric@gmail.com>
2020-11-28 21:26:16 +0200

    added the main file for to manage the game

--- game_of_life.py
+++ game_of_life.py
@@ -1,13 +1,14 @@
-from life import life
+"""
+    This program implements the Game of Life (https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life)
+    We assumme that cells at the edges of the board are dead.
+"""
+
+from life import boards as br
 
 def main():
-    # TODO ask user for the type of spaceship they want to see playing
-    # Play with the appropriate ship -> here dummy HeavyWeight 
-    board = life.HeavyWeightShip(20, 20)
-    activate_cells(board, board.live_cells)
-    gen = next_gen(board)
-
-    # TODO ask for the number of generation for Life
+    board = br.HeavyWeightShip(15, 15)
+    br.activate_cells(board, board.live_cells)
+    gen = br.next_gen(board)
 
     for _ in range(5):
         print('\n----------------\n')
