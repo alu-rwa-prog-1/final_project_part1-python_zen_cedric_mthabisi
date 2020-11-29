eb550beb6c47a38483ebb736b28915d43a1786e7
Mthabisi Ndlovu <m.ndlovu@alustudent.com>
2020-11-29 19:42:20 +0200

    Added a text-based user interface.

--- life/life.py
+++ life/life.py
@@ -4,14 +4,42 @@
 """
 
 import boards
+import sys
+
+def select_board():
+    boards_ = [boards.Glider, boards.LightWeightShip, boards.MiddleWeightShip, boards.HeavyWeightShip]
+    print("Boards: \n"
+          "1. Glider\n"
+          "2. Light Weight Ship\n"
+          "3. Middle Weight Ship\n"
+          "4. Heavy Weight Ship\n")
+
+    bd_index = int(input("Enter a corresponding number to choose a board: ")) - 1
+
+    if bd_index < 0 or bd_index > 3:
+        raise IndexError("Could not find a board with that index.")
+
+    def make_board(width, height):
+        print("Creating your board...")
+        board = boards_[bd_index](width, height)
+        print("Done!")
+        return board
+
+    return make_board
 
 def main():
-    board = boards.HeavyWeightShip(15, 15)
+    print("Welcome to Conway's Game of Life, select a board to start the game.")
+    make_board = select_board()
+    n = int(input("How many rows or columns should your board have? "))
+    board = make_board(n, n)
     gen = boards.next_gen(board)
+    nxt = 'next'
 
-    for _ in range(5):
-        print('\n----------------\n')
+    while nxt.lower() == 'next':
+        print('\n{}\n'.format('--' * board.height))
         print(next(gen))
+        nxt = input("< type next to see the next generation > OR < type anything you want to quit >")
+    sys.exit()
 
 
 if __name__ == '__main__':
